b002b4d61452fee526be2726ad8a5c051ab80679
CollectInte <info@collectintel.in>
2026-02-23 16:48:00 +0530

    updatedenv

--- src/StaffComponents/services/api.js
+++ src/StaffComponents/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://gym.caredesk360.com",
 
   withCredentials: true, // 🔥 sends cookie automatically
 });
